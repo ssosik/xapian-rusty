dbf546466d5d7c2137d63de7072b4a94884472a9
ssosik <ssosik@users.noreply.github.com>
2021-08-09 10:06:08 -0400

    Statically link to zlib and xapian-core (#1)

--- build.rs
+++ build.rs
@@ -16,8 +16,12 @@ fn main() {
         .file("xapian-bind.cc")
         .flag_if_supported("-std=c++14")
         .flag_if_supported("-Wno-deprecated-declarations")
+        .static_flag(true)
+        .include("xapian-core-1.4.17/include")
         .compile("xapian-rusty");
 
-    println!("cargo:rustc-link-lib=xapian");
+    println!("cargo:rustc-flags=-lz");
+    println!("cargo:rustc-link-search=all=xapian-core-1.4.17/.libs");
+    println!("cargo:rustc-link-lib=static=xapian");
     println!("cargo:rustc-link-lib=m");
 }
